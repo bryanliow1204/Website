7f93d507eae73f23bf5209b8506a921c0a94b2ab
bryanliow1204 <44526505+bryanliow1204@users.noreply.github.com>
2020-05-27 20:10:43 +0800

    Add files via upload

---
+++ BLL/ManageChecklist.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAF_Website.BLL
+{
+    //Insert the checklist
+    public class InsertChecklist
+    {
+        [JsonProperty("checklist_name")]
+        public string checklist_name { get; set; }
+
+        [JsonProperty("created_checklist")]
+        public string created_checklist { get; set; }
+
+        [JsonProperty("user_id")]
+        public int user_id { get; set; }
+    }
+
+    public class UpdateChecklistName 
+    {
+        [JsonProperty("checklist_name")]
+        public string checklist_name { get; set; }
+
+        [JsonProperty("checklist_id")]
+        public int checklist_id { get; set; }
+    }
+
+    public class getChecklistName
+    {
+        [JsonProperty("checklist_id")]
+        public int checklist_id { get; set; }
+
+        [JsonProperty("checklist_name")]
+        public string checklist_name { get; set; }
+
+        public getChecklistName(int checklist_id, string checklist_name)
+        {
+            this.checklist_id = checklist_id;
+            this.checklist_name = checklist_name;
+          
+        }
+    }
+
+    public class ManageChecklist
+    {
+        [JsonProperty("checklist_id")]
+        public int checklist_id { get; set; }
+
+        [JsonProperty("checklist_name")]
+        public string checklist_name { get; set; }
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("created_checklist")]
+        public string created_checklist { get; set; }
+
+    
+        //Default Constructor
+        public ManageChecklist()
+        {
+
+        }
+
+        public ManageChecklist(int checklist_id, string checklist_name, string name, string created_checklist)
+        {
+            this.checklist_id = checklist_id;
+            this.checklist_name = checklist_name;
+            this.name = name;
+            this.created_checklist = created_checklist;
+
+        }
+    }
+}
---
+++ BLL/Participants.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAF_Website.BLL
+{
+    public class ListParticipants
+    {
+        public List<Participants> Participants { get; set; }
+    }
+    public class Participants
+    {
+        [JsonProperty("activity_id")]
+        public int activity_id { get; set; }
+
+        [JsonProperty("activity_date")]
+        public string activity_date { get; set; }
+
+        [JsonProperty("activity_name")]
+        public string activity_name { get; set; }
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("group_name")]
+        public string group_name { get; set; }
+
+        [JsonProperty("checklist_name")]
+        public string checklist_name { get; set; }
+
+        // Default constructor
+        public Participants()
+        {
+            
+        }
+
+        public Participants(int id, string activitydate, string activityname, string name, string group, string checklistName)
+        {
+          
+            activity_id = id;
+            activity_date = activitydate;
+            activity_name = activityname;
+            this.name = name;
+            group_name = group;
+            checklist_name = checklistName;
+
+        }
+
+    }
+
+}
